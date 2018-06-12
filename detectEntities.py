f96a8fb0770763576bced6cdb403e8d0ab34eb91
Sunny Miglani <sm15504@my.bristol.ac.uk>
2018-06-12 10:55:14 +0100

    added entities sorting from the first bits of much ado about nothing

--- detectEntities.py
+++ detectEntities.py
@@ -2,8 +2,20 @@ import boto3
 import json
 
 comprehend = boto3.client(service_name='comprehend', region_name='eu-west-1')
-text = "It is raining today in Seattle"
+textFile = open("much_ado_about_nothing.txt", "r", encoding="utf8");
+text = textFile.read();
+
+
 
 print('Calling DetectEntities')
-print(json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+stringResponse = json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4) # type string
+dictResponse = json.loads(stringResponse);
+listOfEntities = [];
+listOfDefines = [];
+for entity in dictResponse['Entities']:
+    listOfEntities.append(entity['Text']);
+    listOfDefines.append(entity['Type']);
+    print("Entity : {0}, Type : {1}".format(entity['Text'], entity['Type']));
+
 print('End of DetectEntities\n')
+
